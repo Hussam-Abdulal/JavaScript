997ab4dfbf9923c1269f24102d08eb17188d602d
Hussam Abdulal <hussam.abdulal@medieinstitutet.se>
2024-01-19 19:39:32 +0100

    add coursers.js index.html

---
+++ coursers.js
@@ -0,0 +1,28 @@
+const coursers = [
+    {
+        title: 'JavaScript for eperts',
+        number: 2522
+    },
+    {
+        title: 'ddddd for eperts',
+        number: 7865
+    },
+    {
+        title: 'Jaaaa for eperts',
+        number: 4566
+    },
+    
+
+]
+
+function initPage() {
+    listCourser();
+}
+
+function listCourser() {
+    coursers.forEach((course) => {
+        console.log(course);
+    });
+    
+}
+ document.addEventListener('DomContentLoad', initPage);
